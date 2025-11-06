0bf8c795e64a34f37d01e98500e763910d0352e1
Lokesh <240003374@kluniversity.in>
2025-11-06 17:29:56 +0530

    added file and folders

--- src/Components/Admissions.jsx
+++ src/Components/Admissions.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { AiOutlineArrowLeft } from "react-icons/ai";
-import "./admissions.css";
+import "./Admissions.css";
 import {
   LineChart,
   Line,
